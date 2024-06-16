12262bd232e7af0f9b66fcd0a7a6b20e2be69b9b
Bomachi <bomeoutlook@gmail.com>
2024-06-16 19:18:17 +0700

    FIx:Movement bug

--- Assets/Script/Player/Player Controller.cs
+++ Assets/Script/Player/Player Controller.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour   {
     public float moveSpeed;
-
     private bool isMoving;
 
     private Vector2 input;
@@ -15,54 +13,57 @@ public class PlayerController : MonoBehaviour
     public LayerMask solidObjectLayer;
     public LayerMask interactablesLayer;
 
-    private void Awake()
-    {
+    private void Awake(){
         animator = GetComponent<Animator>();
     }
 
+    private void Update(){
+        HandleUpdate();
+    }
     public void HandleUpdate(){
-    if (!isMoving){
+        if (!isMoving){
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            if (input.x != 0) input.y = 0;
+        if (input.x != 0) input.y = 0;
 
-            if (input != Vector2.zero)
-            {
-                animator.SetFloat("moveX", input.x);
-                animator.SetFloat("moveY", input.y);
+        if (input != Vector2.zero)
+        {
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
 
-                var targetPos = transform.position;
-                targetPos.x += input.x/16;
-                targetPos.y += input.y/16;
+            var targetPos = transform.position;
+            targetPos.x += input.x/16;
+            targetPos.y += input.y/16;
 
-                if(IsWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));
+            if(IsWalkable(targetPos)){
+                StartCoroutine(Move(targetPos));
             }
-
-
-
         }
 
-        animator.SetBool("isMoving", isMoving);
 
-        if (Input.GetKeyDown(KeyCode.F)){
-            Interact();
-        }
 
-        void Interact(){
-            
-            var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
-            var interactPos = transform.position + facingDir;
-            var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactablesLayer);
+    }
 
-            if (collider != null){
-                collider.GetComponent<Interactable>()?.Interact();
-            }
+    animator.SetBool("isMoving", isMoving);
 
+    if (Input.GetKeyDown(KeyCode.F)){
+        Interact();
+    }
+
+    void Interact(){
+        
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactablesLayer);
+
+        if (collider != null){
+            collider.GetComponent<Interactable>()?.Interact();
         }
 
     }
+
+}
 
     IEnumerator Move(Vector3 targetPos){
 
@@ -86,7 +87,7 @@ public class PlayerController : MonoBehaviour
         }
 
         return true;
-
+        } 
     }
 
-}
+
